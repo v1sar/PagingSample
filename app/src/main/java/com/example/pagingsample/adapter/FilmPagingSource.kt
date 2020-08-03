39178240bf5a5411ee4b4d0afb0e05cd52f6ee28
Dmitry Movchan <dmitry.movchan@revolut.com>
2020-08-03 13:40:52 +0300

    add loging

--- app/src/main/java/com/example/pagingsample/adapter/FilmPagingSource.kt
+++ app/src/main/java/com/example/pagingsample/adapter/FilmPagingSource.kt
@@ -1,5 +1,6 @@
 package com.example.pagingsample.adapter
 
+import android.util.Log
 import androidx.paging.PagingSource
 import com.example.pagingsample.data.DataProvider
 import com.example.pagingsample.data.FilmModel
@@ -7,6 +8,9 @@ import com.example.pagingsample.data.FilmModel
 class FilmPagingSource : PagingSource<Int, FilmModel>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, FilmModel> {
         val offset = params.key ?: 0
+
+        Log.d(FilmPagingSource::class.java.simpleName, "params.key = ${params.key} params.loadSize = ${params.loadSize}")
+
         val data = DataProvider.getData(offset, params.loadSize)
 
         return LoadResult.Page(
